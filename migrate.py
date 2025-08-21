f42e74685fbf0a8c860e73285920f111591f074f
jecatatu6 <clickcorpsolution@gmail.com>
2025-08-21 07:42:56 -0300

    Update migrate.py

--- migrate.py
+++ migrate.py
@@ -2,15 +2,12 @@ import subprocess
 import sys
 
 def run_migrations():
-    try:
-        print("Executando migrações do Prisma...")
-        result = subprocess.run(["prisma", "migrate", "deploy"], check=True, capture_output=True, text=True)
-        print(result.stdout)
-        print("Migrações aplicadas com sucesso.")
-    except subprocess.CalledProcessError as e:
-        print("Erro ao aplicar migrações:")
-        print(e.stderr)
-        sys.exit(1)
+    print("Executando migrações do Prisma...")
+    subprocess.run(
+        [sys.executable, "-m", "prisma", "migrate", "deploy"],
+        check=True
+    )
+    print("Migrações aplicadas com sucesso.")
 
 if __name__ == "__main__":
     run_migrations()
